d1f60be4fbb2cb1573c2eec6c5269d36af557d76
derekfle <derekfletcher5@hotmail.com>
2016-01-18 22:40:49 -0800

    Basic blobs in fluid sim

--- FluidSIm/FluidController.cpp
+++ FluidSIm/FluidController.cpp
@@ -1,20 +1,39 @@
 #include "FluidController.h"
 #include "ResourceManager.h"
 
-FluidController::FluidController()
-{
-}
+#include <iostream>
 
+FluidController::FluidController() :
+	Scale(5.f / GridSize),
+	Voxel(nullptr)
+{
+	Grid.resize(GridSize);
+	for (unsigned i = 0; i < GridSize; i++)
+	{
+		Grid[i].resize(GridSize);
+		for (unsigned j = 0; j < GridSize; j++)
+		{
+			Grid[i][j].resize(GridSize);
+		}
+	}
+
+	Blobs.push_back(Blob(glm::vec3(GridToWorldX(GridSize / 2), GridToWorldY(GridSize / 2), GridToWorldZ(GridSize / 2))));
+	Blobs.push_back(Blob(glm::vec3(GridToWorldX(GridSize / 2), GridToWorldY(GridSize / 2), GridToWorldZ(GridSize / 2))));
+	Blobs.push_back(Blob(glm::vec3(GridToWorldX(GridSize / 2), GridToWorldY(GridSize / 2), GridToWorldZ(GridSize / 2))));
+}
 
 FluidController::~FluidController()
 {
+	if (Voxel) delete Voxel;
 }
 
 void FluidController::Initialize()
 {
 	// Increase screen dimensions and then set the camera location
 	SetScreenDimensions(1920, 1080);
-	SetCamera(glm::vec3(0.f, 0.f, 10.0f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.0f, 0.f));
+	SetCamera(glm::vec3(8.f, 8.f, 10.0f), glm::vec3(1.f, 0.f, 0.f), glm::vec3(0.f, 1.0f, 0.f)); // Real
+	//SetCamera(glm::vec3(75.f, 55.f, 150.0f), glm::vec3(10.f, 20.f, 0.f), glm::vec3(0.f, 1.0f, 0.f)); // 64
+	//SetCamera(glm::vec3(75.f, 55.f, 100.0f), glm::vec3(10.f, 20.f, 0.f), glm::vec3(0.f, 1.0f, 0.f)); // 32
 
 	// OpenGL configuration
 	glEnable(GL_CULL_FACE);
@@ -24,10 +43,51 @@ void FluidController::Initialize()
 
 	// Initialize the shader to be used for the masses
 	RESOURCEMANAGER.LoadShader("basic.vs", "basic.fs", nullptr, "Basic");
+	RESOURCEMANAGER.GetShader("Basic").SetVector4f("inColor", glm::vec4(0.f, 0.75f, 1.f, 1.f), true);
+
+	Voxel = new Cube();
+	Voxel->SetShader("Basic");
+	Voxel->SetScale(glm::vec3(Scale, Scale, Scale));
 }
 
 void FluidController::Update(const GLdouble& dt)
 {
+	// Translate the metaballs based on their velocities
+	for (GLuint i = 0; i < Blobs.size(); i++)
+	{
+		GLuint gx = WorldToGridX(Blobs[i].Position.x),
+			gy = WorldToGridY(Blobs[i].Position.y),
+			gz = WorldToGridZ(Blobs[i].Position.z);
+
+		if (gx == 0 || gx >= GridSize - 1)
+		{
+			Blobs[i].Velocity.x = -Blobs[i].Velocity.x;
+		}
+
+		if (gy == 0 || gy >= GridSize - 1)
+		{
+			Blobs[i].Velocity.y = -Blobs[i].Velocity.y;
+		}
+
+		if (gz == 0 || gz >= GridSize - 1)
+		{
+			Blobs[i].Velocity.z = -Blobs[i].Velocity.z;
+		}
+
+		Blobs[i].Position += Blobs[i].Velocity * static_cast<float>(dt);
+	}
+
+	// Compute field strength at each
+	for (GLuint i = 0; i < GridSize; i++)
+	{
+		for (GLuint j = 0; j < GridSize; j++)
+		{
+			for (GLuint k = 0; k < GridSize; k++)
+			{
+				Grid[i][j][k] = ComputeVoxel(i, j, k);
+			}
+		}
+	}
 }
 
 void FluidController::ProcessInput(const GLdouble& dt)
@@ -36,4 +96,90 @@ void FluidController::ProcessInput(const GLdouble& dt)
 
 void FluidController::Render()
 {
+	auto Draw = [this](const GLint& i, const GLint& j, const GLint& k)
+	{
+		Voxel->SetTranslation(glm::vec3(i * Scale, j * Scale, k * Scale));
+		//Voxel->SetTranslation(glm::vec3(i, j, k));
+		Voxel->Draw(View, Projection);
+	};
+
+	for (GLint i = 0; i < GridSize; i++)
+	{
+		for (GLint j = 0; j < GridSize; j++)
+		{
+			for (GLint k = 0; k < GridSize; k++)
+			{
+				/*if (Grid[i][j][k] >= 1.f)
+				{
+					//Voxel->SetTranslation(glm::vec3(i * Scale, j * Scale, k * Scale));
+					//Voxel->Draw(View, Projection);
+					//Draw(i, j, k);
+				}*/
+
+				if (Grid[i][j][k] >= 1.f)
+				{
+					if (i == 0 || i == GridSize - 1 ||
+						j == 0 || j == GridSize - 1 ||
+						k == 0 || k == GridSize - 1)
+					{
+						Draw(i, j, k);
+					}
+					else
+					{
+						if (Grid[i + 1][j][k] < 1.f ||
+							Grid[i - 1][j][k] < 1.f ||
+							Grid[i][j + 1][k] < 1.f ||
+							Grid[i][j - 1][k] < 1.f ||
+							Grid[i][j][k + 1] < 1.f ||
+							Grid[i][j][k - 1] < 1.f)
+						{
+							Draw(i, j, k);
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+GLfloat FluidController::ComputeVoxel(const GLuint& gx, const GLuint& gy, const GLuint& gz) const
+{
+	GLfloat val = 0, WorldX = GridToWorldX(gx), WorldY = GridToWorldY(gy), WorldZ = GridToWorldZ(gz);
+
+	for (unsigned i = 0; i < Blobs.size(); i++)
+	{
+		val += glm::pow(Blobs[i].Radius, 2) / (glm::pow(WorldX - Blobs[i].Position.x, 2) + glm::pow(WorldY - Blobs[i].Position.y, 2) + glm::pow(WorldZ - Blobs[i].Position.z, 2));
+	}
+
+	return val;
+}
+
+GLfloat FluidController::GridToWorldX(const GLuint& gx) const
+{
+	return gx * Scale;
+}
+
+GLfloat FluidController::GridToWorldY(const GLuint& gy) const
+{
+	return gy * Scale;
+}
+
+GLfloat FluidController::GridToWorldZ(const GLuint& gz) const
+{
+	return gz * Scale;
+}
+
+GLuint FluidController::WorldToGridX(const GLfloat& wx) const
+{
+	return wx / Scale;
+}
+
+GLuint FluidController::WorldToGridY(const GLfloat& wy) const
+{
+	return wy / Scale;
+}
+
+GLuint FluidController::WorldToGridZ(const GLfloat& wz) const
+{
+	return wz / Scale;
 }
--- FluidSIm/FluidController.h
+++ FluidSIm/FluidController.h
@@ -2,6 +2,11 @@
 
 #include "GameController.h"
 
+#include "Cube.h"
+
+#include <glm/gtc/random.hpp>
+#include <vector>
+
 /*
 * Sets up and showcases several different springs
 */
@@ -18,5 +23,35 @@ public:
 	void Update(const GLdouble& dt);
 	void ProcessInput(const GLdouble& dt);
 	void Render();
+
+private:
+
+	// Structure to contain blob data
+	struct Blob
+	{
+		Blob(const glm::vec3& GridPos) : Radius(1.f), Position(GridPos)
+		{
+			Velocity = glm::vec3(glm::linearRand(-0.5f, 0.5f), glm::linearRand(-0.5f, 0.5f), glm::linearRand(-0.5f, 0.5f));
+			//Velocity = glm::vec3(.25f, 0.f, 0.f);
+		};
+		GLfloat Radius;
+		glm::vec3 Position, Velocity;
+	};
+
+	GLfloat ComputeVoxel(const GLuint& gx, const GLuint& gy, const GLuint& gz) const;
+
+	GLfloat GridToWorldX(const GLuint& gx) const;
+	GLfloat GridToWorldY(const GLuint& gy) const;
+	GLfloat GridToWorldZ(const GLuint& gz) const;
+
+	GLuint WorldToGridX(const GLfloat& gx) const;
+	GLuint WorldToGridY(const GLfloat& gy) const;
+	GLuint WorldToGridZ(const GLfloat& gz) const;
+
+	const GLuint GridSize = 32;
+	std::vector<std::vector<std::vector<GLfloat>>> Grid;
+	std::vector<Blob> Blobs;
+	GLfloat Scale;
+	Cube* Voxel;
 };
 
