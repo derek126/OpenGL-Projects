e1cb0863743b791a114dfb4c383251ec700db471
derekfle <derekfletcher126@gmail.com>
2016-02-20 18:25:16 -0800

    Change to opengl 4.3

--- OpenGLFramework/GameManager.cpp
+++ OpenGLFramework/GameManager.cpp
@@ -17,7 +17,7 @@ GameManager::GameManager() :
 		exit(EXIT_FAILURE);
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
