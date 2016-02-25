a9ac234c179f5dc93fd712c5653e5d1a4758890c
derekfle <derekfletcher126@gmail.com>
2016-02-24 22:15:33 -0800

    Basic setup

--- Lagrangian/LagrangianController.cpp
+++ Lagrangian/LagrangianController.cpp
@@ -1,4 +1,8 @@
 #include "LagrangianController.h"
+#include "ResourceManager.h"
+
+#define ScreenX 1920
+#define ScreenY 1080
 
 LagrangianController::LagrangianController()
 {
@@ -12,7 +16,29 @@ LagrangianController::~LagrangianController()
 
 void LagrangianController::Initialize()
 {
+	GameController::Initialize();
+	GAMEMANAGER.SetTargetFrametime(60);
 
+	// Increase screen dimensions and then set the camera location
+	SetScreenDimensions(ScreenX, ScreenY);
+
+	Camera->SetPosition(glm::vec3(45.f, 10.f, 45.0f));
+	Camera->SetFocus(glm::vec3(0.5f, 0.5f, 0.f));
+	Camera->SetWorldUp(glm::vec3(0.f, 1.f, 0.f));
+	Camera->UpdateView();
+
+	// Hide the mouse cursor
+	glfwSetInputMode(Window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	// Set the directional light direction and color
+	RESOURCEMANAGER.SetLightDirection(glm::vec3(1.f, 1.f, 1.f));
+	RESOURCEMANAGER.SetLightColor(glm::vec3(1.f, 1.f, 1.f));
+
+	// OpenGL configuration
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_DEPTH_TEST);
 }
 
 void LagrangianController::Update(const GLdouble& dt)
@@ -27,7 +53,22 @@ void LagrangianController::ProcessInput(const GLint& Key, const GLint& Action, c
 
 void LagrangianController::ProcessMouseMove(const GLdouble& dX, const GLdouble& dY)
 {
+	static GLint normalX = ScreenX / 2;
+	static GLint normalY = ScreenY / 2;
 
+	GLboolean bDidUpdate = false;
+	if (dX != normalX || dY != normalY)
+	{
+		Camera->RotateByMouse(static_cast<GLfloat>(dX - normalX) * 0.0005f, static_cast<GLfloat>(dY - normalY) * 0.0005f);
+		Camera->UpdateView();
+
+		bDidUpdate = true;
+	}
+
+	if (bDidUpdate)
+	{
+		glfwSetCursorPos(Window, normalX, normalY);
+	}
 }
 
 void LagrangianController::Render()
